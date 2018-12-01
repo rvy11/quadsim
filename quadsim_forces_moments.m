c5705381460f07e8c84312ae8eb82ec1829d9dfe
Robin <ryohannan@embue.com>
2018-12-01 16:56:18 -0500

    Project Step #3 (Complete) - Implemented forces and moments

--- quadsim_forces_moments.m
+++ quadsim_forces_moments.m
@@ -54,10 +54,48 @@ function out = quadsim_forces_moments(uu, P)
     % Your code goes below...
     
     % Prop rotation rates
-    omega_1 = 0;  % Propeller 1 rotation rate, rad/s (function of P.prop_1_omega_bias, etc.)
+    omega_1 = P.k_omega*delta_1 + P.prop_1_omega_bias;  % Propeller 1 rotation rate, rad/s (function of P.prop_1_omega_bias, etc.)
+    omega_2 = P.k_omega*delta_2 + P.prop_2_omega_bias;
+    omega_3 = P.k_omega*delta_3 + P.prop_3_omega_bias;
+    omega_4 = P.k_omega*delta_4 + P.prop_4_omega_bias;
     
-    f_b=[0;0;0];
-    m_b=[0;0;0];
+    Tp_1 = P.k_Tp*(omega_1^2);
+    Tp_2 = P.k_Tp*(omega_2^2);
+    Tp_3 = P.k_Tp*(omega_3^2);
+    Tp_4 = P.k_Tp*(omega_4^2);
+    
+    R_ned2b = eulerToRotationMatrix(phi,theta,psi);
+    wind_b = R_ned2b*wind_ned;
+    va_b = vg_b-wind_b;
+    Vair_in = -(va_b(3));
+    Fp_1 = P.rho*P.C_prop*P.S_prop ...
+        *(Vair_in + (omega_1/P.k_omega)*(P.k_motor - Vair_in)) ...
+        *((omega_1/P.k_omega)*(P.k_motor - Vair_in));
+    
+    Fp_2 = P.rho*P.C_prop*P.S_prop ...
+        *(Vair_in + (omega_2/P.k_omega)*(P.k_motor - Vair_in)) ...
+        *((omega_2/P.k_omega)*(P.k_motor - Vair_in));
+    
+    Fp_3 = P.rho*P.C_prop*P.S_prop ...
+        *(Vair_in + (omega_3/P.k_omega)*(P.k_motor - Vair_in)) ...
+        *((omega_3/P.k_omega)*(P.k_motor - Vair_in));
+    
+    Fp_4 = P.rho*P.C_prop*P.S_prop ...
+        *(Vair_in + (omega_4/P.k_omega)*(P.k_motor - Vair_in)) ...
+        *((omega_4/P.k_omega)*(P.k_motor - Vair_in));
+    
+    dy = P.L_to_motor*cos(pi/4);
+    dx = P.L_to_motor*sin(pi/4);
+    m_x = (dy*(Fp_3 + Fp_2) - dy*(Fp_1 + Fp_4));
+    m_y = (dx*(Fp_1 + Fp_3) - dx*(Fp_2 + Fp_4));
+    m_z = ((Tp_1 + Tp_2) - (Tp_3 + Tp_4));
+    
+    f_grav_b = P.mass*R_ned2b*[0; 0; P.gravity];
+    f_props_b = [0;0;-(Fp_1+Fp_2+Fp_3+Fp_4)];
+    f_b_rotorDrag = -P.mu_rotorDrag*[vg_b(1)-wind_b(1);vg_b(2)-wind_b(2);0];
+    
+    f_b=f_grav_b+f_props_b+f_b_rotorDrag;
+    m_b=[m_x;m_y;m_z];
     
     % Compile function output
     out = [f_b; m_b]; % Length 3+3=6
